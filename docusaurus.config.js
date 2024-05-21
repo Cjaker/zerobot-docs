8f68a0eba95c65cc7a9bfd6e18736f2a6666427c
Cjaker <eternalscripts@gmail.com>
2024-05-21 13:11:49 -0300

    Fix docs thumbnail

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/zerobot-social-card.png',
       colorMode: {
         defaultMode: "dark",
       },
